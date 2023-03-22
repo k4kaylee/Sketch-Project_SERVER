9e14aac3c482a6cbf1cde621de1e6f3168f14692
k4kaylee <roman.kulpekshark@gmail.com>
2023-03-22 16:57:58 +0100

    Handling post for users

--- app.js
+++ app.js
@@ -1,17 +1,30 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
 const app = express();
+const bodyParser = require('body-parser')
 const PORT = 5500;
 
 const userRoutes = require('./routes/users');
 
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 app.use('/users', userRoutes);
-
+    
 app.use((req, res, next) => {
-  res.status(200).json({
-    message: 'API works correctly'
-  });
-})
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+       error:{
+        message: error.message
+       }
+     })
+    }
+);
 
  app.listen(PORT, () => console.log('API is running on ' + PORT));
  module.exports = app;
--- routes/users.js
+++ routes/users.js
@@ -3,14 +3,43 @@ const router = express.Router();
 
 router.get('/', (req, res, next) =>{
     res.status(200).json({
-        message: 'Handling GET for users'
+        message: 'Users were fetched'
     })
 });
 
+router.get('/:userId', (req, res, next) => {
+    const id = req.params.userId;
+    if(id === 'special')
+        res.status(200).json({
+            message: "Special ID was handled"
+        });
+    else
+        res.status(200).json({
+            message: "ID was handeled"
+        });
+});
+
 router.post('/', (req, res, next) =>{
-    res.status(200).json({
-        message: 'Handling POST for users'
+    const user = {
+        login: req.body.login,
+        password: req.body.password,
+        email: req.body.email
+    }
+    res.status(201).json({
+        message: 'Handling POST for users',
+        newUser: user
     })
 });
+
+router.patch('/:userId', (req, res, next) => {
+    res.status(200).json({
+        message: "Updated user"
+    })
+})
+router.delete('/:userId', (req, res, next) => {
+    res.status(200).json({
+        message: "User was deleted"
+    })
+})
 
 module.exports = router;
